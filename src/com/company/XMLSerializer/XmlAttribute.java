45a9157cae087ba2486333e2b94b6dd02c870f6a
Arthas_Men <minearthas@gmail.com>
2018-12-01 11:09:10 +0300

    Initial commit

---
+++ src/com/company/XMLSerializer/XmlAttribute.java
@@ -0,0 +1,13 @@
+package com.company.XMLSerializer;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface XmlAttribute {
+  String tag() default "";
+  String name() default "";
+}
---
+++ src/com/company/XMLSerializer/XmlTag.java
@@ -0,0 +1,12 @@
+package com.company.XMLSerializer;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface XmlTag {
+  String name() default "";
+}
